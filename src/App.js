e1df830b7e7550d9f1b9942ff66f0769da5054f9
mzhou08 <mzhou08@gmail.com>
2020-09-05 19:56:21 -0400

    added calls to the backend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import { Grid } from '@material-ui/core';
 import './App.css';
 import GoogleButton from './components/GoogleButton';
+import ZoomButton from './components/ZoomButton';
 
-function App() {
-  return (
-    <Grid container style={{width: '100%', marginTop: '2em'}}>
-      <Grid item xs={10} />
-      <Grid item xs={2} style={{display: 'flex', justifyContent: 'left'}} >
-          <GoogleButton/> 
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .catch(err => err);
+  }
+
+  componentDidMount() {
+      this.callAPI();
+  }
+
+  render() {
+
+    return (
+      <Grid container style={{width: '100%', marginTop: '2em'}}>
+        <Grid item xs={2} >
+          <p className="App-intro">{this.state.apiResponse}</p>
+        </Grid>
+        <Grid item xs={4} style={{display: 'flex', justifyContent: 'center'}} >
+            <ZoomButton/> 
+        </Grid>
+        <Grid item xs={4} style={{display: 'flex', justifyContent: 'right'}} >
+            <GoogleButton/> 
+        </Grid>
       </Grid>
-    </Grid>
-  
-  );
+    
+    );
+  }
 }
 
 export default App;
